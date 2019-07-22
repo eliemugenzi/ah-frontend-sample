c27fcdf697c44e10b0855675d11d1b4361b32338
Elie Mugenzi <eliemugenzi@Elies-MacBook-Pro.local>
2019-07-22 08:16:17 +0200

    Updates

---
+++ precache-manifest.20fe6fc2ae73303d8dceff221ad88c6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96ab9eb572fd8a9b98915a385daa781",
+    "url": "/ah-frontend-sample/index.html"
+  },
+  {
+    "revision": "060e5539d4f9f5cff53a",
+    "url": "/ah-frontend-sample/static/css/2.27172684.chunk.css"
+  },
+  {
+    "revision": "bf25ab31d8c42eed4288",
+    "url": "/ah-frontend-sample/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "060e5539d4f9f5cff53a",
+    "url": "/ah-frontend-sample/static/js/2.ccc297bc.chunk.js"
+  },
+  {
+    "revision": "bf25ab31d8c42eed4288",
+    "url": "/ah-frontend-sample/static/js/main.0ddf4e3c.chunk.js"
+  },
+  {
+    "revision": "62cfa16a6b4268ea3d3e",
+    "url": "/ah-frontend-sample/static/js/runtime~main.b82378bb.js"
+  }
+]);
